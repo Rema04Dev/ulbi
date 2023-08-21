8d3a2246c7afcca1a3725a5bccbacb03dc30451f
Ryzhikov Roman <romansmerdyakov@gmail.com>
2023-08-21 19:01:39 +0600

    dev: add mini-css-extract-plugin to webpack

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,5 +1,7 @@
 import webpack from 'webpack';
-export function buildLoaders(): webpack.RuleSetRule[] {
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { BuildOptions } from 'config/types/config';
+export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
   // Если не использовать typescript, то нужно было бы установить babel-loader
   const typescriptLoader = {
     test: /\.tsx?$/,
@@ -11,7 +13,7 @@ export function buildLoaders(): webpack.RuleSetRule[] {
     test: /\.s[ac]ss$/i,
     use: [
       // Creates `style` nodes from JS strings
-      'style-loader',
+      options.isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
       // Translates CSS into CommonJS
       'css-loader',
       // Compiles Sass to CSS
--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import webpack from 'webpack';
 import HTMLWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BuildOptions } from 'config/types/config';
 
 export function buildPlugins({
@@ -9,6 +10,10 @@ export function buildPlugins({
   return [
     new HTMLWebpackPlugin({
       template: paths.html,
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash:8].css',
+      chunkFilename: 'css/[name].[contenthash:8].css',
     }),
     new webpack.ProgressPlugin(),
   ];
